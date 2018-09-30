4a53826b1a5aa4b8332e238f1bee7b689de1bdee
Chidi Williams <chidi.williams@outlook.com>
2018-09-30 21:34:56 +0100

    added random page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from './containers/Container';
+import { Link, Route } from 'react-router-dom';
+import Random from './components/Random';
+
+const Home = () => <div>Home</div>;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Container>
+            <div className="menubar">
+              <h1 className="App-title">Pijin.ng</h1>
+              <ul className="menu">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/entries/random">Random</Link>
+                </li>
+              </ul>
+            </div>
+          </Container>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Route exact path="/entries/random" component={Random} />
+        <Route exact path="/" component={Home} />
       </div>
     );
   }
---
+++ src/components/WordCard.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './WordCard.css';
+import arrow from '../Arrow.svg';
+
+class WordCard extends Component {
+  static propTypes = {
+    entry: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      meaning: PropTypes.string.isRequired,
+      example: PropTypes.string,
+    }).isRequired,
+  };
+
+  render() {
+    const { entry } = this.props;
+
+    return (
+      <div className="card">
+        <div className="card-votes">
+          <a href="#" className="vote-button upvote">
+            <img src={arrow} alt="upvote" />
+          </a>
+          <div className="vote-count upvote"> {entry.votesCount.up}</div>
+          <div className="divider" />
+          <div className="vote-count downvote"> {entry.votesCount.down}</div>
+          <a href="#" className="vote-button upvote">
+            <img className="vote-button downvote" src={arrow} alt="downvote" />
+          </a>
+        </div>
+        <div className="card-entry">
+          <h2 className="entry-name">{entry.name}</h2>
+          <div className="entry-meaning">{entry.meaning}</div>
+          <div className="entry-example">{entry.example}</div>
+        </div>
+        <div className="card-entry-image">
+          <img
+            className="entry-image-image"
+            src="https://picsum.photos/600/300"
+            alt={entry.name}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WordCard;
